4e224ea830f56307c4d5e0fd3cee4972d762edb3
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-22 21:48:57 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
 def key_for_min_value(name_hash)
   name_hash.collect do |name, num|
-    min = num[0]
-    if num < min
-      min = name
+    lowest = num[0]
+    if num < lowest
+      lowest = name
     end
   end
-  return min
+  return lowest
 end
